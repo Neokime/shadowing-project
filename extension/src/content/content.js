fa6be4804d3237391cee3f46cd6c9bd59e16769b
DESKTOP-40JSM55\it <hyunbingun1234@gmail.com>
2026-01-02 16:43:36 +0900

    content 수정

--- extension/src/content/content.js
+++ extension/src/content/content.js
@@ -3,25 +3,23 @@ function getVideo() {
   return document.querySelector('video');
 }
 
-// getvideo() null일때 오류처리
+// video가 이미 있으면 감지만
 const video = getVideo();
-if (!video) return;
-video.play();
+if (video) {
+  console.log('비디오 이미 존재');
+}
 
-
+// video가 없으면 기다렸다가 감지만
 function waitForVideo(callback) {
   const interval = setInterval(() => {
     const video = document.querySelector('video');
-
     if (video) {
       clearInterval(interval);
       callback(video);
     }
-  }, 300); // 0.3초마다 확인
+  }, 300);
 }
-
 
 waitForVideo((video) => {
   console.log('비디오 찾음:', video);
-  video.play();
 });
